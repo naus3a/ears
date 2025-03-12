5fdb5f67b3e416ee2aef789e897f5b86c15e9e74
naus3a <naus3a@gmail.com>
2025-03-12 18:05:31 +0100

    vad listeners

--- src/Ears.js
+++ src/Ears.js
@@ -1,17 +1,53 @@
 class Ears{
     constructor(){
         this.vad = null;
+        this.speechStartListeners = [];
+        this.speechStopListeners = [];
     }
 
     async startListening(){
         this.vad = await vad.MicVAD.new({
             onSpeechStart: () => {
-                console.log("Speech start detected")
+                this.handleSpeechStartCallbacks();
             },
             onSpeechEnd: (audio) => {
-                console.log("speech stopped");
+                this.handleSpeechStopCallbacks();
             }
         });
         this.vad.start();
+    }
+
+    ///
+    /// listeners
+    ///
+
+    addListener(callback, callbackList){
+        callbackList.push(callback);
+    }
+
+    addSpeechStartListener(callback){
+        this.addListener(callback, this.speechStartListeners);
+    }
+
+    addSpeechStopListener(callback){
+        this.addListener(callback, this.speechStopListeners);
+    }
+
+    ///
+    /// handlers
+    ///
+
+    handleCallback(callbackList){
+        for(let i=0;i<callbackList.length;i++){
+            callbackList[i]();
+        }
+    }
+
+    handleSpeechStartCallbacks(){
+        this.handleCallback(this.speechStartListeners);
+    }
+
+    handleSpeechStopCallbacks(){
+        this.handleCallback(this.speechStopListeners);
     }
 }
