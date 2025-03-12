54d7b833ef21ed4b91d2166bf5f05e4e6c050495
naus3a <naus3a@gmail.com>
2025-03-12 18:48:20 +0100

    use web speech API

--- src/Ears.js
+++ src/Ears.js
@@ -1,12 +1,43 @@
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const SpeechGrammarList =
+  window.SpeechGrammarList || window.webkitSpeechGrammarList;
+const SpeechRecognitionEvent =
+  window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
+
 class Ears{
     constructor(){
         this.vad = null;
+        this.recognition = null;
+
         this.speechStartListeners = [];
         this.speechStopListeners = [];
+        this.recognitionResultListeners = [];
     }
 
     async startListening(){
-        this.vad = await vad.MicVAD.new({
+        this.recognition = new SpeechRecognition();
+        this.recognition.continuous = false;
+        this.recognition.lang = "en-US";
+        this.recognition.interimResults = false;
+        this.recognition.maxAlternatives = 1;
+
+        this.recognition.onspeechstart = () => {
+            this.handleSpeechStartCallbacks();
+        }
+
+        this.recognition.onspeechend = () => {
+            this.handleSpeechStopCallbacks();
+        };
+
+        this.recognition.onresult = (event)=>{
+            this.handleRecognitionResultCallbacks(event.results[0][0].transcript);
+            //console.log(event.results[0][0].transcript);
+        };
+
+        this.recognition.start();
+
+        /*this.vad = await vad.MicVAD.new({
             onSpeechStart: () => {
                 this.handleSpeechStartCallbacks();
             },
@@ -14,7 +45,13 @@ class Ears{
                 this.handleSpeechStopCallbacks();
             }
         });
-        this.vad.start();
+        this.vad.start();*/
+    }
+
+    stopListening(){
+        if(this.recognition!==null){
+            this.recognition.stop();
+        }
     }
 
     ///
@@ -33,6 +70,10 @@ class Ears{
         this.addListener(callback, this.speechStopListeners);
     }
 
+    addRecognitionResultListener(callback){
+        this.addListener(callback, this.recognitionResultListeners);
+    }
+
     ///
     /// handlers
     ///
@@ -43,11 +84,21 @@ class Ears{
         }
     }
 
+    handleArgCallback(arg, callbackList){
+        for(let i=0;i<callbackList.length;i++){
+            callbackList[i](arg);
+        }
+    }
+
     handleSpeechStartCallbacks(){
         this.handleCallback(this.speechStartListeners);
     }
 
     handleSpeechStopCallbacks(){
         this.handleCallback(this.speechStopListeners);
+    }
+
+    handleRecognitionResultCallbacks(res){
+        this.handleArgCallback(res, this.recognitionResultListeners);
     }
 }
